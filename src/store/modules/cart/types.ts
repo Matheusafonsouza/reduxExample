77417aab8f19d39d1ef08c6a38e11c5ae4bf99d5
Matheus Afonso <matheusafonsouza@gmail.com>
2020-11-22 21:04:28 -0300

    feat: created types for cart reducer

---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+}
+
+export interface ICardItem {
+  product: IProduct;
+  quantity: number;
+}
+
+export interface ICartState {
+  items: ICardItem[];
+}
