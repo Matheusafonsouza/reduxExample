f9a620ef3e228e531a10f3d5d92ea917e8af71ca
Matheus Afonso <matheusafonsouza@gmail.com>
2020-11-22 21:57:02 -0300

    feat: config for redux dev tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,12 @@
 import { createStore } from 'redux';
 import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export interface IState {
   cart: ICartState;
 }
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
