a7d7fd729100d3eb70e2c7d84da8c626592b4e57
Matheus Afonso <matheusafonsouza@gmail.com>
2020-12-30 21:30:09 -0300

    fix: using redux saga to create middlewares for store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './modules/rootSaga';
 
 export interface IState {
   cart: ICartState;
 }
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(...middlewares)
+));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
