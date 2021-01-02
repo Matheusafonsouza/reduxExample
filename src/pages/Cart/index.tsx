823d65ddf456a5b3686961d8a57fcfcf013a5193
Matheus Afonso <matheusafonsouza@gmail.com>
2021-01-01 22:39:12 -0300

    fix: created cart components

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import Catalog from '../../components/Catalog';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, CartDetail, CartItems, CartDetailHeader, CartDetailInfo } from './styles';
 
 function Cart() {
 
   return (
-    <>
-      <Container>
-      </Container>
-    </>
+    <Container>
+      <CartDetail>
+        <CartDetailHeader>
+          <Link to="/">Voltar</Link>
+          <strong>Carrinho</strong>
+        </CartDetailHeader>
+
+        <CartDetailInfo>
+          <h1>Total: R$ 400,00</h1>
+          <button>Comprar</button>
+        </CartDetailInfo>
+      </CartDetail>
+      <CartItems>
+        <Catalog />
+      </CartItems>
+    </Container>
   );
 }
 
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -4,5 +4,86 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
 
+  display: flex;
+
   background: #523628;
 `
+
+export const CartDetail = styled.div`
+  height: 100%;
+  width: 30%;
+  padding: 30px;
+  background: #F8AF00;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CartDetailHeader = styled.div`
+  height: 20%;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    padding: 10px;
+    border: 0;
+    background: #5F463A;
+    color: #fff;
+    text-decoration: none;
+
+    border-radius: 5px;
+    margin-right: 10px;
+
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+
+  strong {
+    font-size: 40px;
+    color: #5F463A;
+  }
+`;
+
+export const CartDetailInfo = styled.div`
+  height: 80%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 40px;
+    color: #5F463A;
+  }
+
+  button {
+    padding: 10px;
+    border: 0;
+    background: #5F463A;
+    color: #fff;
+    text-decoration: none;
+
+    border-radius: 5px;
+    margin-right: 10px;
+
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+`;
+
+export const CartItems = styled.div`
+  height: 100%;
+  width: 70%;
+  background: transparent;
+`;
